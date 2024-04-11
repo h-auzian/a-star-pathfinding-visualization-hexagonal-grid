5c058e6a096923d4ff20cafc6fedf05e7979fb3d
Hans Auzian C <h.auzian@gmail.com>
2024-04-11 18:37:02 -0400

    refactor: split draw function into several steps

--- js/draw.js
+++ js/draw.js
@@ -1,11 +1,22 @@
 import state from "./state.js";
 
 function draw(canvas, context, info) {
+    clear(canvas, context);
+    applyTransformations(context);
+    drawScene(context);
+    updateInfo(info);
+}
+
+function clear(canvas, context) {
     context.setTransform(1, 0, 0, 1, canvas.width/2, canvas.height/2);
     context.clearRect(-canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
+}
 
+function applyTransformations(context) {
     context.translate(-state.camera.x, -state.camera.y);
+}
 
+function drawScene(context) {
     const squareSize = 50;
     context.fillStyle = "red";
     for (let j = 0; j < 5; j++) {
@@ -15,7 +26,9 @@ function draw(canvas, context, info) {
             context.fillRect(positionX, positionY, squareSize, squareSize);
         }
     }
+}
 
+function updateInfo(info) {
     info.innerHTML = "Canvas: " + Math.round(canvas.width) + "x" + Math.round(canvas.height) + "<br>";
     info.innerHTML += "Camera: " + Math.round(state.camera.x) + "x" + Math.round(state.camera.y) + "<br>";
 }
